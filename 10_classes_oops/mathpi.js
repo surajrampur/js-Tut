225eb60da1e44ca2fba8f6a7f0e747b8790d433f
SURAJ SAINI <140495640+surajrampur@users.noreply.github.com>
2024-06-15 17:32:59 +0000

    done

---
+++ 10_classes_oops/mathpi.js
@@ -0,0 +1,36 @@
+const descriptor= Object.getOwnPropertyDescriptor(Math,"PI")
+
+// console.log(Math.PI)
+// Math.PI=5
+// console.log(Math.PI)
+
+
+// console.log(descriptor)
+
+const chai={
+    name:"ginger chai",
+    price:250,
+    isAvailable:true,
+
+    orderChai:function(){
+        console.log(`chai nahi bani`)
+    }
+}
+
+// console.log(chai)
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+
+Object.defineProperty(chai, "name",{
+    writable:false,
+    enumerable:false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+
+
+for (let [key,value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key}:${value}`)
+    }
+}
